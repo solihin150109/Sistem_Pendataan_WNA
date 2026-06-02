4c5d0ffcd83d7e193052bb51c332385aeb83af95
solihin150109 <uhuywawak@gmail.com>
2026-06-02 14:01:51 +0700

    fix: update root path in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './client',
+    root: './',
   build: {
     outDir: './client/dist',
     emptyOutDir: true
